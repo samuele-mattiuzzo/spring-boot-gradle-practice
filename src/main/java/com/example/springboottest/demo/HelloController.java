f52d46a98eab353cbac2290101e1cd124e0aa8d3
Samuele Mattiuzzo <samumatt@gmail.com>
2022-05-03 17:26:47 +0100

    Converted app to hello, world with custom named greeting

--- src/main/java/com/example/springboottest/demo/HelloController.java
+++ src/main/java/com/example/springboottest/demo/HelloController.java
@@ -1,12 +1,18 @@
 package com.example.springboottest.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloController {
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+
+    @GetMapping("/hello")
+    public String sayHello(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
--- src/test/java/com/example/springboottest/demo/DemoApplicationTests.java
+++ src/test/java/com/example/springboottest/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.springboottest.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-class DemoApplicationTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
+
+	@Autowired
+	private TestRestTemplate template;
 
 	@Test
-	void contextLoads() {
+	public void index_ok() throws Exception {
+		ResponseEntity<String> response = template.getForEntity("/", String.class);
+		assertThat(response.getBody()).contains("Get your greeting");
 	}
 
 }
